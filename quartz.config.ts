1c4735fc8fe2e1978ebab12602fe8cb28c39db08
Kai Boschung <kai@appery.ch>
2025-01-04 19:43:17 +0100

    Quartz sync: Jan 4, 2025, 7:43 PM

--- quartz.config.ts
+++ quartz.config.ts
@@ -16,7 +16,7 @@ const config: QuartzConfig = {
       provider: "plausible",
     },
     locale: "de-DE",
-    baseUrl: "engineererr.github.io/daddydocs/",
+    baseUrl: "www.daddydocs.ch",
     ignorePatterns: ["private", "templates", ".obsidian"],
     defaultDateType: "created",
     generateSocialImages: false,
