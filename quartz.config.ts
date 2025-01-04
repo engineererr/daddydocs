85c5da835213e9ed32f5b60c524802cf3774c5eb
Kai Boschung <kai@appery.ch>
2025-01-04 19:27:59 +0100

    Quartz sync: Jan 4, 2025, 7:27 PM

--- quartz.config.ts
+++ quartz.config.ts
@@ -8,15 +8,15 @@ import * as Plugin from "./quartz/plugins"
  */
 const config: QuartzConfig = {
   configuration: {
-    pageTitle: "🪴 Quartz 4.0",
-    pageTitleSuffix: "",
+    pageTitle: "🧔‍♂️Daddydocs",
+    pageTitleSuffix: "Vom Kinderkriegen",
     enableSPA: true,
     enablePopovers: true,
     analytics: {
       provider: "plausible",
     },
-    locale: "en-US",
-    baseUrl: "quartz.jzhao.xyz",
+    locale: "de-DE",
+    baseUrl: "https://engineererr.github.io/daddydocs/",
     ignorePatterns: ["private", "templates", ".obsidian"],
     defaultDateType: "created",
     generateSocialImages: false,
